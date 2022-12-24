5fe3ab785e90eba9b82031cf2f31452b67083962
Sonu Kumar <114944221+sonukumar121@users.noreply.github.com>
2022-12-24 16:18:15 +0530

    Add files via upload

---
+++ equal no.or not ..c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  printf("Enter the value of x= ");
+  scanf("%d",&x);
+  printf("Enter the value of y= ");
+  scanf("%d",&y);
+  if(x==y)
+  {
+    printf("both are equal number");
+  }
+  else
+  printf("both are not equal");
+  getch();
+  return 0;
+
+}
