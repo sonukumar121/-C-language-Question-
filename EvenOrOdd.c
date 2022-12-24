fc70825a945912e20e47ce5f2d6ca3b73c1afe29
Sonu Kumar <114944221+sonukumar121@users.noreply.github.com>
2022-12-24 16:57:43 +0530

    Add files via upload

---
+++ EvenOrOdd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+// odd,even;
+int main()
+{
+   int n;
+   printf("Enter any number= ");
+   scanf("%d",&n);
+   if(n%2==0)
+   {
+      printf("even number");
+
+   }
+   else
+   printf("odd number");
+   getch();
+   return 0;
+
+}
