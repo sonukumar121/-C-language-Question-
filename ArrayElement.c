ff68d9865f0d6abbb356f2354992f6c7bcff39bf
Sonu Kumar <114944221+sonukumar121@users.noreply.github.com>
2022-12-25 13:54:06 +0530

    Add files via upload

---
+++ ArrayElement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   int a[5],i;
+   printf("Enter array elements= ");
+   for(i=0;i<=4;i++)
+   {
+     scanf("%d",&a[i]);
+   }
+   for(i=0;i<=4;i++)
+   {
+     printf("%d",a[i]);
+   }
+   getch();
+   return 0;
+ }
+
+
+
+
+
