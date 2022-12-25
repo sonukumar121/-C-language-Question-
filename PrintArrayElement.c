8cfab5dd3bc171e3afecff9d1e0b4d5457de3d89
Sonu Kumar <114944221+sonukumar121@users.noreply.github.com>
2022-12-25 17:11:31 +0530

    Add files via upload

---
+++ PrintArrayElement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a[6],i;
+   printf("Enter array elements= \n");
+   for(i=0;i<=6;i++)
+   {
+     scanf("%d",&a[i]);
+   }
+   printf("the sum of array element is:= ");
+   for(i=0;i<=6;i++)
+   {
+
+    printf("%d",a[i]);
+   }
+
+
+
+
+}
