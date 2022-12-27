e56dd828e59b9845eaf3c1c099119e53d28d9f9e
Sonu Kumar <114944221+sonukumar121@users.noreply.github.com>
2022-12-27 15:33:39 +0530

    Add files via upload

---
+++ GreaterNumber.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int x,y,z;
+   printf("Enter the number of x=");
+   scanf("%d",&x);
+   printf("Enter the value of y= ");
+   scanf("%d",&y);
+   z=(x>y)? printf("x  is greater than y") :printf("y is greater than x");
+
+
+}
